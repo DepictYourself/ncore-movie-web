0746b7a5312c46affc85e610e883f011bfbc0383
DepictYourself <totha014@gmail.com>
2023-02-08 13:55:13 +0100

    Remove angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,6 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
