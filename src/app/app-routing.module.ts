ea7612a683a602023d2f9190b8af2f374f9272cd
DepictYourself <totha014@gmail.com>
2023-02-09 11:56:28 +0100

    implement basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieComponent } from './movie/movie.component';
+import { TorrentBrowserComponent } from './torrent-browser/torrent-browser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: "", component: TorrentBrowserComponent},
+    {path: "torrents", component: TorrentBrowserComponent},
+    {path: "movie", component: MovieComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
