ef1147fddd4309e61d8acc4c83d77da0211d6670
DepictYourself <totha014@gmail.com>
2023-02-15 09:37:27 +0100

    add route for download manager component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DownloadManagerComponent } from './download-manager/download-manager.component';
 import { MovieComponent } from './movie/movie.component';
 import { TorrentBrowserComponent } from './torrent-browser/torrent-browser.component';
 
 const routes: Routes = [
     {path: "", component: TorrentBrowserComponent},
     {path: "torrents", component: TorrentBrowserComponent},
-    {path: "movie/:imdbId", component: MovieComponent}
+    {path: "movie/:imdbId", component: MovieComponent},
+    {path: "downloads", component: DownloadManagerComponent}
 ];
 
 @NgModule({
