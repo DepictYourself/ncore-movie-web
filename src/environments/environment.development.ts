8a77a7ad78a78542d932f7b7d725806c0145a0c8
DepictYourself <totha014@gmail.com>
2023-02-20 16:25:01 +0100

    generate environement files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    ncoreApiUrl: "http://localhost:5000/torrents",
+    tmdbApiUrl: "http://localhost:5005/tmdb"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    ncoreApiUrl: "https://my-json-server.typicode.com/DepictYourself/ncore-movie-web/torrents",
+    tmdbApiUrl: "https://node-api-key-proxy-production.up.railway.app/tmdb"
+};
