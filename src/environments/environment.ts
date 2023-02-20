5beb6f7c935f6f190481c9b72ac8d3f7383eecd5
DepictYourself <totha014@gmail.com>
2023-02-20 16:57:59 +0100

    add production: true to environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production: true,
     ncoreApiUrl: "https://my-json-server.typicode.com/DepictYourself/ncore-movie-web/torrents",
     tmdbApiUrl: "https://node-api-key-proxy-production.up.railway.app/tmdb"
 };
